cdc11e14f008a3a06d692c7401b4cfd3bf50df31
Ian Young <ian@iangreenleaf.com>
2025-02-15 22:58:13 -0600

    Update Postgrator call, use single connection string instead

--- packages/db/migrate.ts
+++ packages/db/migrate.ts
@@ -2,53 +2,21 @@
 import chalk from 'chalk'
 import * as dotenv from 'dotenv'
 import Postgrator from 'postgrator'
+import pg from 'pg'
 
 dotenv.config()
 
 const log = (text: string, style: typeof chalk.white = chalk.white): void =>
   console.log(`${chalk.cyanBright('>')} ${style(text)}`)
 
-interface DBEnv {
-  host: string
-  port: number
-  database: string
-  username: string
-  password: string
-}
-
-const getEnv = (): DBEnv => {
-  const {
-    PG_HOST: host,
-    PG_PORT: port,
-    PG_DB: database,
-    PG_USER: username,
-    PG_PASSWORD: password,
-  } = process.env
-
-  if (typeof username !== 'string') {
-    throw new Error('No PG user passed in env')
-  }
-
-  if (typeof password !== 'string') {
-    throw new Error('No PG password passed in env')
-  }
-
-  const config = {
-    host: host || '127.0.0.1',
-    port: port ? parseInt(port, 10) : 5432,
-    database: database || 'omnivore',
-    username,
-    password,
-    options: process.env.PG_EXTRA_OPTIONS,
-  }
-
-  return config
-}
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const postgrator = new Postgrator({
   migrationDirectory: __dirname + '/migrations',
   driver: 'pg',
-  ...getEnv(),
+  execQuery: (query) => client.query(query),
   // Schema table name
   schemaTable: 'schemaversion',
   // Validate migration md5 checksum to ensure the contents of the script have not changed
@@ -85,8 +53,8 @@ const logAppliedMigrations = (
 }
 
 // postgres migration
-const postgresMigration = postgrator
-  .migrate(targetMigration)
+const postgresMigration = client.connect()
+  .then(() => postgrator.migrate(targetMigration))
   .then(logAppliedMigrations)
   .catch((error) => {
     log(
